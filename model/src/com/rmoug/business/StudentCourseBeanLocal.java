f3a9bfab548430f66cf273e8e533c86a67df28a2
Bigyan Acharya <bigyan.acharya@pinnacol.com>
2014-12-04 17:06:46 -0700

    initial project creation

---
+++ model/src/com/rmoug/business/StudentCourseBeanLocal.java
@@ -0,0 +1,42 @@
+package com.rmoug.business;
+
+import com.rmoug.entities.Course;
+import com.rmoug.entities.Student;
+import com.rmoug.entities.StudentCourse;
+
+import java.util.List;
+
+import javax.ejb.Local;
+
+@Local
+public interface StudentCourseBeanLocal {
+    Object queryByRange(String jpqlStmt, int firstResult, int maxResults);
+
+    <T> T persistEntity(T entity);
+
+    <T> T mergeEntity(T entity);
+
+    Course persistCourse(Course course);
+
+    Course mergeCourse(Course course);
+
+    void removeCourse(Course course);
+
+    List<Course> getCourseFindAll();
+
+    Student persistStudent(Student student);
+
+    Student mergeStudent(Student student);
+
+    void removeStudent(Student student);
+
+    List<Student> getStudentFindAll();
+
+    StudentCourse persistStudentCourse(StudentCourse studentCourse);
+
+    StudentCourse mergeStudentCourse(StudentCourse studentCourse);
+
+    void removeStudentCourse(StudentCourse studentCourse);
+
+    List<StudentCourse> getStudentCourseFindAll();
+}
---
+++ service/src/com/rmoug/service/StudentService.java
@@ -0,0 +1,47 @@
+package com.rmoug.service;
+
+import com.rmoug.business.StudentCourseBeanLocal;
+
+import com.rmoug.entities.Student;
+import com.rmoug.entities.StudentCourse;
+
+import java.util.List;
+
+import javax.inject.Singleton;
+
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+
+import javax.ejb.EJB;
+
+
+@Singleton
+@Path("service")
+public class StudentService {
+    
+    @EJB
+    StudentCourseBeanLocal StudentBean;
+    
+    public StudentService() {
+        super();
+    }
+
+    @GET
+    @Produces("application/json")
+    @Path("/hello")
+    public String helloWorld(){
+        
+        return "Hello RMOUG 2015";
+    }
+    
+    @GET
+    @Produces("application/json")
+    @Path("/student")
+    public List<Student> getStudentFindAll(){
+        
+        return StudentBean.getStudentFindAll();
+        
+    }
+    
+}
