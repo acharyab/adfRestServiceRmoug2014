4c40f8ec2733cd2b844530ed094848083d081b7d
Bigyan Acharya <bigyan.acharya@pinnacol.com>
2015-02-16 13:09:57 -0700

    code clean up

--- service/src/com/rmoug/service/StudentService.java
+++ service/src/com/rmoug/service/StudentService.java
@@ -51,19 +51,16 @@ public class StudentService {
         return "Hello RMOUG 2015";
     }
     
+    //Get all courses
+    
     @GET
-    @Produces("application/json")
-    @Path("/student")
-    public List<Student> getStudentFindAll(@QueryParam("studentId") Integer studentId){    
-        List<Student> students;
-        if (studentId != null){
-            students = studentBean.getStudentById(studentId);
-        }else{
-            students = studentBean.getStudentFindAll();
-        }
-        
-        return students;       
+    @Produces(MediaType.APPLICATION_JSON)
+    @Path("/course")
+    public List<Course> getAllCourses(){
+        return studentBean.getCourseFindAll();
     }
+    
+    //Get students enrolled in a course.
     
     @GET
     @Produces(MediaType.APPLICATION_JSON)
@@ -80,65 +77,7 @@ public class StudentService {
       return studentsForCourse;
     }
     
-    
-    @POST
-    @Consumes (MediaType.APPLICATION_JSON)
-    @Path("/studentCourse")
-    public Response addStudentToCourse(StudentCourse studentCourse) {
-        StudentCourse newSc= new StudentCourse();
-        newSc = studentBean.persistStudentCourse(studentCourse);
-        return Response.ok().build();
-    } 
-    
-
-    @DELETE
-    @Consumes (MediaType.APPLICATION_JSON)
-    @Path("/studentCourse")
-    public Response removeStudentFromCourse(StudentCourse studentCourse) {
-        studentBean.removeStudentCourse(studentCourse);
-        return Response.ok().build();
-    }  
-
-    @DELETE
-    @Path("/studentCourse/{studentCourseId}")
-    public Response removeStudentFromCourseParam(@PathParam("studentCourseId") String studentCourseId) {
-        StudentCourse sc = new StudentCourse();
-        sc.setId(new BigDecimal(studentCourseId));
-        studentBean.removeStudentCourse(sc);
-        return Response.ok().build();
-    }     
-    
-    @PUT
-    @Consumes(MediaType.APPLICATION_JSON)
-    @Path("/student")
-    public Response updateStudent(Student student){
-       studentBean.mergeStudent(student);
-        return Response.ok().build(); 
-    }
-    
-    
-    @GET
-    @Produces(MediaType.APPLICATION_JSON)
-    @Path("/course")
-    public List<Course> getAllCourses(){
-        return studentBean.getCourseFindAll();
-    }
-    
-    @GET
-    @Produces(MediaType.APPLICATION_JSON)
-    @Path("/coursesForStudent") 
-    public List<StudentCourseVw> getCoursesForStudent(@QueryParam("studentId") BigDecimal studentId) {
-        
-      List<StudentCourseVw> courseForStudent;
-      
-      if (studentId != null)
-          courseForStudent = studentBean.getCoursesByStudent(studentId);
-      else
-          throw new WebApplicationException(Response.Status.BAD_REQUEST);
-      
-      return courseForStudent;
-    }
-    
+    //Get students not enrolled in a course.
     @GET
     @Produces(MediaType.APPLICATION_JSON)
     @Path("/studentsNotEnrolled") 
@@ -154,4 +93,73 @@ public class StudentService {
       return studentNotEnrolled;
     }
     
+    //Get Student by ID, if null then return all students.
+    
+    @GET
+    @Produces("application/json")
+    @Path("/student")
+    public List<Student> getStudentFindAll(@QueryParam("studentId") Integer studentId){    
+        List<Student> students;
+        if (studentId != null){
+            students = studentBean.getStudentById(studentId);
+        }else{
+            students = studentBean.getStudentFindAll();
+        }       
+        return students;       
+    }
+    
+ 
+    //Get all courses taken by a student.
+    @GET
+    @Produces(MediaType.APPLICATION_JSON)
+    @Path("/coursesForStudent") 
+    public List<StudentCourseVw> getCoursesForStudent(@QueryParam("studentId") BigDecimal studentId) {
+        
+      List<StudentCourseVw> courseForStudent;
+      
+      if (studentId != null)
+          courseForStudent = studentBean.getCoursesByStudent(studentId);
+      else
+          throw new WebApplicationException(Response.Status.BAD_REQUEST);
+      
+      return courseForStudent;
+    }
+    
+    //Update Student info
+    @PUT
+    @Consumes(MediaType.APPLICATION_JSON)
+    @Path("/student")
+    public Response updateStudent(Student student){
+       studentBean.mergeStudent(student);
+        return Response.ok().build(); 
+    }    
+    
+    //Enroll student to a course.
+    @POST
+    @Consumes (MediaType.APPLICATION_JSON)
+    @Path("/studentCourse")
+    public Response addStudentToCourse(StudentCourse studentCourse) {
+        StudentCourse newSc= new StudentCourse();
+        newSc = studentBean.persistStudentCourse(studentCourse);
+        return Response.ok().build();
+    }     
+
+    //Delete student by passing json payload. Does not work with Java 7, fixed with java 8.
+    @DELETE
+    @Consumes (MediaType.APPLICATION_JSON)
+    @Path("/studentCourse")
+    public Response removeStudentFromCourse(StudentCourse studentCourse) {
+        studentBean.removeStudentCourse(studentCourse);
+        return Response.ok().build();
+    }  
+
+    //Delete student by id.
+    @DELETE
+    @Path("/studentCourse/{studentCourseId}")
+    public Response removeStudentFromCourseParam(@PathParam("studentCourseId") String studentCourseId) {
+        StudentCourse sc = new StudentCourse();
+        sc.setId(new BigDecimal(studentCourseId));
+        studentBean.removeStudentCourse(sc);
+        return Response.ok().build();
+    }       
 }
